88c426da0b3a5539976394405802b140e59862f7
Blythe Pollard <blytheepollard@gmail.com>
2019-10-19 18:51:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -79,7 +79,7 @@ end
     puts "Which song number would you like to play?"
     input = gets.strip.to_i #gets input, makes sure it's integer
     if input >= 1 && input <= songs.length
-      songs.all
+      Song.all.sort {|a, b| a.name <=> b.name}
     #sort songs, pull out input - 1
     
     #find the song
