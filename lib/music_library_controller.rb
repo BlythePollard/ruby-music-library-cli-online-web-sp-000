05e29b2d3b3023f3a2f627b2e73f45a7906525a0
Blythe Pollard <blytheepollard@gmail.com>
2019-10-19 18:29:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -78,7 +78,7 @@ end
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i #gets input, makes sure it's integer
-    #checks input is between 1 and songs.length
+    #if input is between 1 and songs.length
     if song = Song.find_by_name(input) #if song input matches existing song (is valid input)
     #iterates over list_songs? but how to iterate over list of strings? why isn't list_songs showing up as list of strings?
     #pulls matching input from list_songs
